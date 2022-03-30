0e6bab1e3398cf02de0759b0baff3a5a9745d7f9
NagyPatrik024 <nagy.patrik024@gmail.com>
2022-03-30 21:47:01 +0200

    last changes

--- Tic-tac-toe/Logic/TicTacToeLogic.cs
+++ Tic-tac-toe/Logic/TicTacToeLogic.cs
@@ -38,28 +38,6 @@ namespace Tic_tac_toe.Logic
                 }
                 
             }
-
-            //for (int k = 0; k < GameMatrix.GetLength(0); k++)
-            //{
-            //    for (int h = 0; h < GameMatrix.GetLength(1); h++)
-            //    {
-            //        if ((GameMatrix[0, 0] == 'X' && GameMatrix[0, 1] == 'X' && GameMatrix[0, 2] == 'X')
-            //            || (GameMatrix[0, 0] == 'X' && GameMatrix[1, 0] == 'X' && GameMatrix[2, 0] == 'X')
-            //            || (GameMatrix[0, 2] == 'X' && GameMatrix[1, 2] == 'X' && GameMatrix[2, 2] == 'X')
-            //            || (GameMatrix[2, 0] == 'X' && GameMatrix[2, 1] == 'X' && GameMatrix[2, 2] == 'X'))
-            //        {
-
-            //        }
-            //        else if ((GameMatrix[0, 0] == 'O' && GameMatrix[0, 1] == 'O' && GameMatrix[0, 2] == 'O')
-            //            || (GameMatrix[0, 0] == 'O' && GameMatrix[1, 0] == 'O' && GameMatrix[2, 0] == 'O')
-            //            || (GameMatrix[0, 2] == 'O' && GameMatrix[1, 2] == 'O' && GameMatrix[2, 2] == 'O')
-            //            || (GameMatrix[2, 0] == 'O' && GameMatrix[2, 1] == 'O' && GameMatrix[2, 2] == 'O'))
-            //        {
-
-            //        }
-            //    }
-            //}
-
         }
 
         public bool IsThereAnyFreePlace()
